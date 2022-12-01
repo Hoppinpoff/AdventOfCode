d77dc5751e0eafc96b7447871b7342f86886d22f
jlee <jlee@JH1PXG2-PC>
2022-12-01 11:33:54 -0500

    Add project files.

---
+++ Day1.CalorieCounting/Program.cs
@@ -0,0 +1,21 @@
+﻿List<int> totalCalories = new();
+string input = "C:/Users/jlee/Desktop/adventinput.txt";
+
+using (StreamReader reader = new(input)) {
+  string? line;
+  int calories = 0;
+  while ((line = reader.ReadLine()) != null) {
+    if (line != "") {
+      calories+=int.Parse(line);
+    } else {
+      totalCalories.Add(calories);
+      calories = 0;
+    }
+    }
+}
+Console.WriteLine(totalCalories.Max().ToString());
+totalCalories.Sort();
+totalCalories.Reverse();
+int finalAnswer = totalCalories[0] + totalCalories[1]+ totalCalories[2];
+Console.WriteLine(finalAnswer);
+
